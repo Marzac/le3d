b04725d7a9f2f93572c02e2398128b339cfc45cb
Frédéric Meslin <fred@fredslab.net>
2018-01-02 17:14:56 +0100

    Simplification of engine configuration flags

--- engine/config.h
+++ engine/config.h
@@ -5,11 +5,11 @@
 	\author Frederic Meslin (fred@fredslab.net)
 	\twitter @marzacdev
 	\website http://fredslab.net
-	\copyright Frederic Meslin 2015 - 2017
-	\version 1.3
+	\copyright Frederic Meslin 2015 - 2018
+	\version 1.4
 
 	The MIT License (MIT)
-	Copyright (c) 2017 Frédéric Meslin
+	Copyright (c) 2015-2018 Frédéric Meslin
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
@@ -42,30 +42,29 @@
 	#define LE_BMPCACHE_SLOTS			1024					/** Maximum number of bitmaps in cache */
 	#define LE_MESHCACHE_SLOTS			1024					/** Maximum number of meshes in cache */
 
-/** Bitmap management */
-	#define LE_BMP_CHECKCOORDS 			1						/** Check coordinates on bitmap operations */
-	#define LE_BMP_MIPMAPS	 			32						/** Maximum mipmap level */
-
 /** Wavefront object parser */
 	#define LE_OBJ_MAX_NAME				256						/** Wavefront object maximum name string length */
 	#define LE_OBJ_MAX_LINE				1024					/** Wavefront object maximum file line length */
 	#define LE_OBJ_MAX_PATH				256						/** Wavefront object maximum path string length */
 
+/** Bitmap manipulator */
+    #define LE_BMP_MIPMAPS              32                      /** Maximum number of mipmaps per bitmap */
+
 /** Renderer configuration */
-	#define LE_RENDERER_FRONT			-0.5f					/** Front clipping plane */
+	#define LE_RENDERER_FRONT			-1.0f					/** Front clipping plane */
 	#define LE_RENDERER_BACK			-32768.0f				/** Back clipping plane */
 	#define LE_RENDERER_FOV				65.0f					/** Default field of view */
 	#define LE_RENDERER_3DFRUSTRUM		1						/** Use a 3D frustrum to clip triangles */
 	#define LE_RENDERER_2DFRAME			0						/** Use a 2D frame to clip triangles */
 
-	#define LE_RENDERER_INTRASTER		0						/** Enable fixed point or floating point rasterizing */
+	#define LE_RENDERER_INTRASTER		0   					/** Enable fixed point or floating point rasterizing */
 	#define LE_RENDERER_MIPMAPS			1						/** Enable mipmapping on textures */
 
 	#define LE_TRILIST_MAX				50000					/** Maximum number of triangles in display list */
 	#define LE_VERLIST_MAX				(3 * LE_TRILIST_MAX)	/** Maximum number of vertexes in transformation buffer */
 
 /** Performance optimizations */
-	#define LE_USE_SIMD					1						/** Use SIMD (SSE / Neon) vectors */
-	#define LE_FORCE_16B_ALIGN			1						/** Force 16bit alignment (needed by SSE) */
+	#define LE_USE_SIMD					1						/** Use SIMD instructions & vectors */
+	#define LE_USE_SSE2					1						/** Use Intel SSE2 instructions */
 
-#endif
+#endif // LE_CONFIG_H
