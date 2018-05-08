a701e063ee578fffd4f7f6e0d7df67ad5ede1761
Frederic Meslin <fred@fredslab.net>
2018-05-08 15:18:44 +0200

    New system class as an abstraction layer to general OS / HW things

---
+++ engine/system.h
@@ -0,0 +1,76 @@
+/**
+	\file system.h
+	\brief LightEngine 3D: Native OS / HW system manager
+	\brief All platforms implementation
+	\author Frederic Meslin (fred@fredslab.net)
+	\twitter @marzacdev
+	\website http://fredslab.net
+	\copyright Frederic Meslin 2015 - 2018
+	\version 1.6
+
+	The MIT License (MIT)
+	Copyright (c) 2015-2018 Frédéric Meslin
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+#ifndef LE_SYSTEM_H
+#define LE_SYSTEM_H
+
+#include "global.h"
+#include "config.h"
+
+/*****************************************************************************/
+/**
+	\enum LE_SYSTEM_STATUS
+	\brief application current status
+*/
+typedef enum {
+	LE_SYSTEM_UNKNOWN			= 0x0,
+	LE_SYSTEM_ALRIGHT			= 0x01,
+	LE_SYSTEM_EXIT_QUIT			= 0x02,
+	LE_SYSTEM_EXIT_ABORT		= 0x04,
+}LE_SYSTEM_STATUS;
+
+/*****************************************************************************/
+/**
+	\class LeSystem
+	\brief Handle OS / HW configuration and servicing
+*/
+class LeSystem
+{
+public:
+	LeSystem();
+	~LeSystem();
+
+	void initialize();
+	void terminate();
+	void update();
+
+	int getStatus();
+	bool running;
+	
+private:
+	int status;
+	friend void SignalHandler(int signal);
+};
+
+extern LeSystem sys;
+
+#endif // LE_SYSTEM_H
---
+++ engine/system_unix.cpp
@@ -0,0 +1,114 @@
+/**
+	\file system_win.cpp
+	\brief LightEngine 3D: Native OS / HW system manager
+	\brief Unix OS implementation
+	\author Frederic Meslin (fred@fredslab.net)
+	\twitter @marzacdev
+	\website http://fredslab.net
+	\copyright Frederic Meslin 2015 - 2018
+	\version 1.6
+
+	The MIT License (MIT)
+	Copyright (c) 2015-2018 Frédéric Meslin
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+#include "system.h"
+
+#include "global.h"
+#include "config.h"
+
+#include <stdlib.h>  
+#include <signal.h>  
+#include <stdio.h>
+
+/*****************************************************************************/
+LeSystem system;
+void SignalHandler(int signal);
+
+/*****************************************************************************/
+LeSystem::LeSystem() :
+	running(false),
+	status(LE_SYSTEM_UNKNOWN)
+{
+}
+
+LeSystem::~LeSystem()
+{
+}
+
+/*****************************************************************************/
+/**
+	\fn void LeSystem::initialize()
+	\brief Initialize native OS / HW specifics
+*/
+void LeSystem::initialize()
+{
+	running = true;
+	status = LE_SYSTEM_ALRIGHT;
+
+//	signal(SIGQUIT, SignalHandler);  
+	signal(SIGTERM, SignalHandler);
+    signal(SIGABRT, SignalHandler);  
+    signal(SIGINT, SignalHandler);
+}
+
+/**
+	\fn void LeSystem::terminate()
+	\brief Terminate native OS / HW specifics
+*/
+void LeSystem::terminate()
+{
+	running = false;
+}
+
+/*****************************************************************************/
+/**
+	\fn void LeSystem::update()
+	\brief Update native OS / HW specifics
+*/
+void LeSystem::update()
+{
+	// Not necessary on Unix OS yet
+}
+
+/*****************************************************************************/
+/**
+	\fn int LeSystem::getStatus()
+	\brief Retrieve native OS / HW current status
+	\return OS status
+*/
+int LeSystem::getStatus()
+{
+	return status;
+}
+
+/*****************************************************************************/
+void SignalHandler(int signal)  
+{  
+    switch (signal) {
+//		case SIGQUIT:	status = LE_SYSTEM_EXIT_QUIT; break;
+		case SIGTERM:	status = LE_SYSTEM_EXIT_QUIT; break;
+		case SIGABRT:	status = LE_SYSTEM_EXIT_ABORT; break;
+		case SIGINT:	status = LE_SYSTEM_EXIT_ABORT; break;
+		default: 		status = LE_SYSTEM_UNKNOWN; break;
+    }
+	running = false;
+}  
