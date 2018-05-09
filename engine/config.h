3eb90c61a7f9034866dd30bb5645540777227b4a
Frederic Meslin <fred@fredslab.net>
2018-05-09 18:11:19 +0200

    Moving executable to subdirs / removing cbp project / setting the config back to defaults

--- engine/config.h
+++ engine/config.h
@@ -59,7 +59,7 @@
 	#define LE_RENDERER_3DFRUSTRUM		1						/** Use a 3D frustrum to clip triangles */
 	#define LE_RENDERER_2DFRAME			0						/** Use a 2D frame to clip triangles */
 
-	#define LE_RENDERER_INTRASTER		1						/** Enable fixed point or floating point rasterizing */
+	#define LE_RENDERER_INTRASTER		0						/** Enable fixed point or floating point rasterizing */
 	#define LE_RENDERER_MIPMAPS			1						/** Enable mipmapping on textures */
 
 	#define LE_TRILIST_MAX				50000					/** Maximum number of triangles in display list */
