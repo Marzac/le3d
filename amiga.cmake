cf631c9e2210fd7cc785c92873900852f6f4384c
Andreas Streichardt <andreas.streichardt@gmail.com>
2018-05-13 15:04:42 +0200

    for fresh compiles: disable compiler checks

--- amiga.cmake
+++ amiga.cmake
@@ -7,6 +7,10 @@ SET(CMAKE_C_COMPILER   "${AMIGA_TOOLCHAIN_PATH}/bin/m68k-amigaos-gcc")
 SET(CMAKE_ASM-VASM_COMPILER "${AMIGA_TOOLCHAIN_PATH}/bin/vasmm68k_mot")
 SET(CMAKE_CXX_COMPILER "${AMIGA_TOOLCHAIN_PATH}/bin/m68k-amigaos-g++")
 
+# newest toolchain works for le3d but the cmake compiler check
+# doesn't work due to linker errors :S
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
 SET(CMAKE_C_FLAGS "-noixemul -m68040" CACHE STRING "" FORCE)
 SET(CMAKE_CXX_FLAGS "-noixemul -m68040" CACHE STRING "" FORCE)
 
