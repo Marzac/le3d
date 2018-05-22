f475cfc7a1332773d92375d6f2d913dcf715234c
Frederic Meslin <fred@fredslab.net>
2018-05-22 19:10:05 +0200

    Update CMake stuff + new example project

--- cmake/Modules/le3d-options.cmake
+++ cmake/Modules/le3d-options.cmake
@@ -1,14 +1,18 @@
 # Engine configuration
-set(LE3D_RESOX_DEFAULT			640				CACHE STRING "Default horizontal resolution")
-set(LE3D_RESOY_DEFAULT			480				CACHE STRING "Default vertical resolution")
-set(LE3D_MAX_FILE_EXTENSION		8				CACHE STRING "Maximum file extension string length")
-set(LE3D_MAX_FILE_NAME			128				CACHE STRING "Maximum file name string length")
-set(LE3D_MAX_FILE_PATH			256				CACHE STRING "Maximum file path string length")
+set(LE3D_RESOX_DEFAULT				640			CACHE STRING "Default horizontal resolution")
+set(LE3D_RESOY_DEFAULT				480			CACHE STRING "Default vertical resolution")
+set(LE3D_MAX_FILE_EXTENSION			8			CACHE STRING "Maximum file extension string length")
+set(LE3D_MAX_FILE_NAME				128			CACHE STRING "Maximum file name string length")
+set(LE3D_MAX_FILE_PATH				256			CACHE STRING "Maximum file path string length")
 mark_as_advanced(LE3D_MAX_FILE_EXTENSION LE3D_MAX_FILE_NAME LE3D_MAX_FILE_PATH)
 
+# Windows manager
+set(LE3D_WINDOW_EXTENDED_KEYS		1			CACHE STRING "Maximum number of mipmaps per bitmap")
+mark_as_advanced(LE3D_WINDOW_EXTENDED_KEYS)
+
 # Data caches
-set(LE3D_BMPCACHE_SLOTS			1024			CACHE STRING "Maximum number of bitmaps in cache")
-set(LE3D_MESHCACHE_SLOTS		1024			CACHE STRING "Maximum number of meshes in cache")
+set(LE3D_BMPCACHE_SLOTS				1024		CACHE STRING "Maximum number of bitmaps in cache")
+set(LE3D_MESHCACHE_SLOTS			1024		CACHE STRING "Maximum number of meshes in cache")
 mark_as_advanced(LE3D_BMPCACHE_SLOTS LE3D_MESHCACHE_SLOTS)
 
 # Wavefront object parser
@@ -26,13 +30,13 @@ set(LE3D_RENDERER_FRONT			    -1.0f		CACHE STRING "Front clipping plane")
 set(LE3D_RENDERER_BACK			    -32768.0f	CACHE STRING "Back clipping plane")
 set(LE3D_RENDERER_FOV				65.0f		CACHE STRING "Default field of view")
 option(LE3D_RENDERER_3DFRUSTRUM		"Use a 3D frustrum to clip triangles" On)
-option(LE3D_RENDERER_2DFRAME        "Use a 2D frame to clip triangles" Off)
+option(LE3D_RENDERER_2DFRAME		"Use a 2D frame to clip triangles" Off)
 
 option(LE3D_RENDERER_INTRASTER "Enable fixed point or floating point rasterizing" Off)
 option(LE3D_RENDERER_MIPMAPS "Enable mipmapping on textures" On)
 
-set(LE3D_TRILIST_MAX				50000				 CACHE STRING "Maximum number of triangles in display list")
-set(LE3D_VERLIST_MAX				150000               CACHE STRING "Maximum number of vertexes in transformation buffer")
+set(LE3D_TRILIST_MAX				50000		CACHE STRING "Maximum number of triangles in display list")
+set(LE3D_VERLIST_MAX				150000		CACHE STRING "Maximum number of vertexes in transformation buffer")
 mark_as_advanced(LE3D_TRILIST_MAX LE3D_VERLIST_MAX)
 
 # Performance optimizations
